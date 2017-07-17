78afbb1f235b82f6ef1afbe9ea50b5611539b419
Tylah Kapa <1005732@Student.sae.edu.au>
2017-07-17 22:38:54 +1000

    A* Stuff
    More changes to scripts, the A* still isn't working completely. There's some offset bullshit happening.

--- Assets/Pathfinding Stuff/PathRequestManager.cs
+++ Assets/Pathfinding Stuff/PathRequestManager.cs
@@ -1,57 +1,73 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System;
+using System.Threading;
 
 public class PathRequestManager : MonoBehaviour {
 
-    Queue<PathRequest> patheRequestQueue = new Queue<PathRequest>();
-    PathRequest currentPathRequest;
+	Queue<PathResult> results = new Queue<PathResult>();
 
-    static PathRequestManager instance;
-    PathFinding pathFinding;
-    bool isProcessing;
+	static PathRequestManager instance;
+	Pathfinding pathfinding;
 
-    private void Awake()
-    {
-        instance = this;
-        pathFinding = GetComponent<PathFinding>();
-    }
+	void Awake() {
+		instance = this;
+		pathfinding = GetComponent<Pathfinding>();
+	}
 
-    public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callBack)
-    {
-        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callBack);
-        instance.patheRequestQueue.Enqueue(newRequest);
-        instance.TryProcessNext();
-    }
+	void Update() {
+		if (results.Count > 0) {
+			int itemsInQueue = results.Count;
+			lock (results) {
+				for (int i = 0; i < itemsInQueue; i++) {
+					PathResult result = results.Dequeue ();
+					result.callback (result.path, result.success);
+				}
+			}
+		}
+	}
 
-    void TryProcessNext()
-    {
-        if (!isProcessing && patheRequestQueue.Count > 0)
-        {
-            currentPathRequest = patheRequestQueue.Dequeue();
-            isProcessing = true;
-            pathFinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
-        }           
-    }
+	public static void RequestPath(PathRequest request) {
+		ThreadStart threadStart = delegate {
+			instance.pathfinding.FindPath (request, instance.FinishedProcessingPath);
+		};
+		threadStart.Invoke ();
+	}
 
-    public void FinishedProcessingPath(Vector3[] path, bool success)
-    {
-        currentPathRequest.callBack(path, success);
-        isProcessing = false;
-        TryProcessNext();
-    }
+	public void FinishedProcessingPath(PathResult result) {
+		lock (results) {
+			results.Enqueue (result);
+		}
+	}
 
-    struct PathRequest {
-        public Vector3 pathStart;
-        public Vector3 pathEnd;
-        public Action<Vector3[], bool> callBack;
 
-        public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
-        {
-            pathStart = _start;
-            pathEnd = _end;
-            callBack = _callback;
-        }
-    }
+
+}
+
+public struct PathResult {
+	public Vector3[] path;
+	public bool success;
+	public Action<Vector3[], bool> callback;
+
+	public PathResult (Vector3[] path, bool success, Action<Vector3[], bool> callback)
+	{
+		this.path = path;
+		this.success = success;
+		this.callback = callback;
+	}
+
+}
+
+public struct PathRequest {
+	public Vector3 pathStart;
+	public Vector3 pathEnd;
+	public Action<Vector3[], bool> callback;
+
+	public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback) {
+		pathStart = _start;
+		pathEnd = _end;
+		callback = _callback;
+	}
+
 }
--- Assets/Pathfinding Stuff/Unit.cs
+++ Assets/Pathfinding Stuff/Unit.cs
@@ -1,82 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Unit : MonoBehaviour {
 
-    public Transform target;
-    float speed = 20;
-    Vector3[] path;
-    Vector3 targetInitPos;
-    int targetIndex;
+	const float minPathUpdateTime = .2f;
+	const float pathUpdateMoveThreshold = .5f;
 
-    private void Start()
-    {
-        targetInitPos = target.position;
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-    }
-    void Update()
-    {
-        if (targetInitPos != target.position && path != null)
-        {
-            new WaitForSeconds(0.5f);
-            targetInitPos = target.position;
-            Array.Clear(path, 0, path.Length);
-            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-        }            
-    }
+	public Transform target;
+	public float speed = 20;
+	public float turnSpeed = 3;
+	public float turnDst = 5;
+	public float stoppingDst = 10;
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
-    {        
-        if (pathSuccessful)
-        {
-            path = newPath;
-            targetIndex = 0;
-            StopCoroutine("FollowPath");
-            StartCoroutine("FollowPath");
-        }
-    }
+	APath path;
 
-    IEnumerator FollowPath()
-    {
-        Vector3 currentWaypoint = path[0];
-        while (true)
-        {
-            if (transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Length)
-                {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
+	void Start() {
+		StartCoroutine (UpdatePath ());
+	}
 
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-            yield return null;
+	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful) {
+		if (pathSuccessful) {
+			path = new APath(waypoints, transform.position, turnDst, stoppingDst);
 
-        }
-    }
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
 
-    public void OnDrawGizmos()
-    {
-        if (path != null)
-        {
-            for (int i = targetIndex; i < path.Length; i++)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one);
+	IEnumerator UpdatePath() {
 
-                if (i == targetIndex)
-                {
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-                else
-                {
-                    Gizmos.DrawLine(path[i - 1], path[i]);
-                }
-            }
-        }
-    }
+		if (Time.timeSinceLevelLoad < .3f) {
+			yield return new WaitForSeconds (.3f);
+		}
+		PathRequestManager.RequestPath (new PathRequest(transform.position, target.position, OnPathFound));
+
+		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+		Vector3 targetPosOld = target.position;
+
+		while (true) {
+			yield return new WaitForSeconds (minPathUpdateTime);
+			print (((target.position - targetPosOld).sqrMagnitude) + "    " + sqrMoveThreshold);
+			if ((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) {
+				PathRequestManager.RequestPath (new PathRequest(transform.position, target.position, OnPathFound));
+				targetPosOld = target.position;
+			}
+		}
+	}
+
+	IEnumerator FollowPath() {
+
+		bool followingPath = true;
+		int pathIndex = 0;
+		transform.LookAt (path.lookPoints [0]);
+
+		float speedPercent = 1;
+
+		while (followingPath) {
+			Vector2 pos2D = new Vector2 (transform.position.x, transform.position.z);
+			while (path.turnBoundaries [pathIndex].HasCrossedLine (pos2D)) {
+				if (pathIndex == path.finishLineIndex) {
+					followingPath = false;
+					break;
+				} else {
+					pathIndex++;
+				}
+			}
+
+			if (followingPath) {
+
+				if (pathIndex >= path.slowDownIndex && stoppingDst > 0) {
+					speedPercent = Mathf.Clamp01 (path.turnBoundaries [path.finishLineIndex].DistanceFromPoint (pos2D) / stoppingDst);
+					if (speedPercent < 0.01f) {
+						followingPath = false;
+					}
+				}
+
+				Quaternion targetRotation = Quaternion.LookRotation (path.lookPoints [pathIndex] - transform.position);
+				transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+				transform.Translate (Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+			}
+
+			yield return null;
+
+		}
+	}
+
+	public void OnDrawGizmos() {
+		if (path != null) {
+			path.DrawWithGizmos ();
+		}
+	}
 }
