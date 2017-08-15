5e041715cc4c1673c136a042cf40656024b26bc0
Tylah Kapa <1005732@Student.sae.edu.au>
2017-08-16 01:33:28 +1000

    Options and Serialization
    Created a class that manages options variables, and a SaveLoadManager that can save and load the on command. The values ARE NOT used anywhere yet. They will need to be integrated at a later date.

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour {
+
+    [Range(0, 2)]
+    [Tooltip("Brightness of the game")]
+    public float brightness = 1.0f;
+
+    [Range(0, 2)]
+    [Tooltip("Contrast of the game")]
+    public float contrast = 1.0f;
+
+    [Range(0, 1)]
+    [Tooltip("Master Volume percentage (0-1)")]
+    public float masterVolume = 1.0f;
+
+    [Range(0, 1)]
+    [Tooltip("Music Volume percentage (0-1)")]
+    public float musicVolume = 1.0f;
+
+    [Range(0, 1)]
+    [Tooltip("Voice Volume percentage (0-1)")]
+    public float voiceVolume = 1.0f;
+
+    [Range(0, 1)]
+    [Tooltip("SFX Volume percentage (0-1)")]
+    public float soundEffectsVolume = 1.0f;
+
+    [Range(0, 100)]
+    [Tooltip("How far does the player need to look to rotate (Degrees)?")]
+    public float lookingAngle = 65.0f;
+
+    [Range(0, 50)]
+    [Tooltip("How fast does the player rotate?")]
+    public float lookingSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        OptionsData o = SaveLoadManager.LoadOptions();
+	}
+
+    //Load in the options for the game
+    void Load(OptionsData o)
+    {
+        brightness = o.options[0];
+        contrast = o.options[1];
+        masterVolume = o.options[2];
+        musicVolume = o.options[3];
+        voiceVolume = o.options[4];
+        soundEffectsVolume = o.options[5];
+        lookingAngle = o.options[6];
+        lookingSpeed = o.options[7];
+    }
+
+    //Called to save the current options configuration
+    void Save()
+    {
+        SaveLoadManager.SaveOptions(this);
+    }
+}
---
+++ Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveLoadManager : MonoBehaviour {
+
+
+    //Should be called when the options are changed and saved
+    public static void SaveOptions(Options _o)
+    {
+        //Make new stuff
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.dataPath + "/GameOptions.opt", FileMode.OpenOrCreate);
+        OptionsData od = new OptionsData(_o);
+
+        //serialize and cclose the stream
+        bf.Serialize(stream, od);
+        stream.Close();
+    }
+
+    //Called whenever the options need to be set (preferrably at the start of the game)
+    public static OptionsData LoadOptions()
+    {
+        //Check if the game files contain the options file
+        if(File.Exists(Application.dataPath + "/GameOptions.opt"))
+        {
+            //Make IO stuff
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.dataPath + "/GameOptions.opt", FileMode.Open);
+            OptionsData o = bf.Deserialize(stream) as OptionsData;
+
+            //Close and return
+            stream.Close();
+            return o;
+        }
+        else
+        { 
+            //Else return the default options
+            Debug.Log("There was no options file on startup");
+            return new OptionsData(new Options());
+        }
+    }
+}
+
+[Serializable]
+public class OptionsData
+{
+    public float[] options;
+
+    //Assign all floats in options
+    public OptionsData(Options _options)
+    {
+        options = new float[8];
+        options[0] = _options.brightness;
+        options[1] = _options.contrast;
+        options[2] = _options.masterVolume;
+        options[3] = _options.musicVolume;
+        options[4] = _options.voiceVolume;
+        options[5] = _options.soundEffectsVolume;
+        options[6] = _options.lookingAngle;
+        options[7] = _options.lookingSpeed;
+    }
+}
